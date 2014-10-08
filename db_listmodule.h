3459a4e3bf212af96273dbfeedebf7c0714d199a
Adam <adwe@live.se>
2014-10-08 22:25:11 +0200

    .h redovisning

--- db_listmodule.h
+++ db_listmodule.h
@@ -1,22 +1,45 @@
+#ifndef ISTRING_H
+#define ISTRING_H
+
 #include <stdio.h>
 
-//Struct of implementation
+//Database consists of this struct.
 struct node;
 
-//Creates an empty node
-struct node empty_node();
+/*
+Input: A pointer to a database created by add_node.
+Effect: Returns a pointer to the value of the node pointed at.
+*/
+char *extract_value(struct node *db);
 
-//Adds an entry (key, value) into the database. 
-void add_node(char input_key[], char input_value[]);
+/*
+Input: A new entry's key, value and pointer to the database you want the new entry in, (key, value, databasepointer). Start a new database by sending in a NULL-pointer as database.
+Effect: Returns a pointer to the specified database with your entry added.
+*/
+struct node *add_node(char input_key[], char input_value[], struct node *db);
 
-//Searches for a matching key in the database and returns whether it is found or not and writes the result to output_buffer.
-char *search_entry(char input_buffer[]);
+/*
+Input: A key and a pointer to the database you want to search for key in.
+Effect: Searches for a matching key in the database. Returns a pointer to the found node if found, else NULL.
+*/
+struct node *search_entry(char input_buffer[], struct node *db);
 
-//Changes the value of a specified key in an entry
-void update_value(char *old_value, char *new_value); 
+/*
+Input: A pointer to the old value and a pointer to the new value.
+Effect: Changes old value into new value. 
+*/
+void *update_value(char *old_value, char *new_value); 
 
-//Searches for input_key, removes the node
-void remove_node(char *input_key, char *output_buffer);
 
-//Prints out the database to the standard ouput
-void print_database();
+
+
+//--------------------------Not good----------------------
+//Prints out the database to the standard output
+void print_database_mod(struct node *db);
+
+
+//Behövs?
+//Creates an empty node.
+struct node *empty_node();
+
+#endif
