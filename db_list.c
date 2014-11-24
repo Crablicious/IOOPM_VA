3eaa471883ec77247917271de0c2dd19855767a8
Victoria <victoria.catalan.r@gmail.com>
2014-11-24 16:53:24 +0100

    org

---
+++ db_list.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//implementationen
+
+extern char buffer[128]; // Globala eller inte?
+
+
+//Datatype key * value * next node
+
+
+//gömma typedef och lägga i .h filen.  
+typedef struct node{
+  char *key;
+  char *value;
+  struct node *next;
+} *Node;
+
+
+Node list = NULL;
+Node cursor = NULL;
+Node current_list = NULL; 
+
+Node empty_node(){
+  Node empty = malloc(sizeof(struct node));
+  return empty;
+}
+
+
+char *clone_string(char *input_string){
+  char *copy = malloc(strlen(input_string)+1);
+  strcpy(copy, input_string);
+  return copy;
+}  
+
+void add_node(char *input_key, char *input_value){
+  Node new_node = empty_node();
+  new_node->key = clone_string(input_key);
+  new_node->value = clone_string(input_value);
+  new_node->next = current_list; //errorrad
+  current_list = new_node; 
+}
+
+
+//case 1
+char *search_entry(char *input_key){
+  cursor = current_list;
+  while(strcmp(input_key, cursor->key) != 0){
+    if (cursor->next){
+    cursor = cursor->next;
+    }
+    else{
+      return NULL;
+    }
+  }
+  return cursor->value;  
+}
+
+
+ 
+//case 2 
+void update_value(char *old_value, char *new_value){
+  strcpy(old_value, new_value);
+}
+  
+
+
+//case 4
+void remove_node(char *input_key, char *output_buffer){
+  int found = 0;
+  cursor = current_list;
+  Node prev = NULL;
+  while(!found && cursor != NULL){
+    if(strcmp(input_key, cursor->key) == 0){
+      if(prev == NULL){ // Delete first node
+        current_list = cursor->next;
+      }else{
+        prev->next = cursor->next;
+      }
+      found = 1;
+      strcpy(output_buffer, cursor->value);
+    }else{
+      prev = cursor;
+      cursor = cursor->next;
+    }
+  }
+}
+
+
+// case 5
+void print_database(){
+  cursor = current_list;
+  while(cursor != NULL){
+    puts(cursor->key);
+    puts(cursor->value);
+    cursor = cursor->next;
+  }
+}
+
+
+
---
+++ db_tree.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node{
+  char *key;
+  char *value;
+  struct node *right;
+noooooooooooode  struct node *left;
+} *Node;
+
+Node cursor = NULL;
+Node current_tree = NULL; 
+
+//Creates an empty tree/node
+Node empty(){
+  Node empty = malloc(sizeof(struct node));
+  return empty;
+}
+
+char *clone_string(char *input_string){
+  char *copy = malloc(strlen(input_string)+1);
+  strcpy(copy, input_string);
+  return copy;
+}
+
+Node create_node(char *input_key, char *input_value){
+  Node new_node = empty();
+  new_node->key = clone_string(input_key);
+  new_node->value = clone_string(input_value);
+  new_node->right = NULL;
+  new_node->left = NULL;
+  return new_node;
+}
+
+Node delete_node(Node *node){
+  if(node != NULL){
+    free(node);
+}
+
+int max (int x, int y){
+  return x > y ? x : y;
+}
+
+int depth(Node tree){
+  if (tree == NULL){
+    return 0;
+  }
+  else{
+    return 1 + max(depth(tree->left), depth(tree->right));
+  }  
+}
+
+int check_parent(Node tree, Node parent){
+  if (0<(strcmp(tree->key, parent->key))){
+    return 1;
+  } 
+  else{
+    return 0;
+  }
+}
+
+void balance(Node tree, Node parent){
+  int r_or_left = check_parent(tree, parent);
+   if (tree->left == NULL){
+    //case 1 right-right-right
+    if (tree->right->left == NULL){
+      tree->right->left=tree;
+
+      if (r_or_left){
+        parent->right=tree->right;
+      }
+      else{
+        parent->left=tree->right;
+      }
+      tree->right=NULL;
+    }
+    //case 2 
+    else{
+      tree->right->right=tree->right->left;
+      tree->right->left=tree;
+      
+      if (r_or_left){
+        parent->right=tree->right;
+      }
+      else{
+        parent->left=tree->right;
+      }
+      
+      tree->right=NULL;
+      tree->left=NULL;
+    }
+  }
+  else{
+  //case 3
+    if (tree->left->right == NULL){
+      tree->left->right=tree;
+
+      if (r_or_left){
+        parent->right=tree->left;
+      }
+      else{
+        parent->left=tree->left;
+      }
+      
+      tree->right=NULL;
+      tree->left=NULL;
+      }
+    //case 4
+    else{
+      tree->left->left=tree->left->right;
+      tree->left->right=tree;
+
+      if (r_or_left){
+        parent->right=tree->left;
+      }
+      else{
+        parent->left=tree->left;
+      }
+      tree->right=NULL;
+      tree->left=NULL;
+    }
+  }
+}
+
+
+//search_entry kollar redan om noden redan finns
+void insert_node(Node new_node, Node *tree, Node parent)
+{
+  if(*tree == NULL){
+    *tree = new_node;
+  }
+  else{
+    int compare = strcmp(new_node->key, (*tree)->key);
+    if(0 < compare){
+       insert_node(new_node, &(*tree)->left, *tree);
+    }
+     else if(0 > compare){
+       insert_node(new_node, &(*tree)->right, *tree);
+     } 
+    if (1 < abs(depth((*tree)->right) - depth((*tree)->left))){
+      balance(*tree, parent);
+    }
+  }
+}
+
+//Adds an entry (key, value) into the database. 
+void add_node(char *input_key, char *input_value)
+{
+  Node new_node = create_node(input_key, input_value);
+  printf("Hej: %s, %s,\n", new_node->key, new_node->value);
+  insert_node(new_node, current_tree, NULL);
+}
+
+
+Node *search_entry(char *input_key)
+{
+  cursor = current_tree;
+  while (cursor != NULL)
+    {
+      int compare = strcmp(input_key, cursor->key);
+      if (compare == 0)
+        {
+          return NULL;
+        }
+      else
+        {
+          cursor = compare > 0 ? cursor->right: cursor->left;
+        }
+    }
+  return cursor->value;
+}
+
+
+
+ 
+//Changes the value of a specified key in an entry
+void update_value(char *old_value, char *new_value)
+{
+  strcpy(old_value, new_value);
+} 
+ 
+ 
+//Searches for input_key, removes the node
+void remove_node(char *input_key, char *output_buffer);
+
+//Prints out the database to the standard ouput
+void print_database();
